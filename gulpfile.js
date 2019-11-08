b828bb87f7d0fdf15d7ad33a8e9e59973cca102d
adhiyudanasvarajati <adhi@10-kai.jp>
2019-11-08 13:59:53 +0900

    header change

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,44 @@ const cleanCss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+const webserver = require('gulp-webserver');
 
 const paths = {
-    src: {
-        scss: 'scss/**/*.scss'
-    },
-    dist: {
-        css: 'css/'
-    }
+  src: {
+    scss: 'scss/**/*.scss'
+  },
+  dist: {
+    css: 'css/'
+  },
+  app: {
+    main: './'
+  }
 };
 
+gulp.task('webserver', function() {
+    gulp.src(paths.app.main)
+      .pipe(webserver({
+        livereload: true,
+        directoryListing: true,
+        open: 'index.html'
+      }));
+  });
+
 gulp.task('sass', done => {
-    gulp.src(paths.src.scss).pipe(sourcemaps.init()).pipe(sass({
-        outputStyle: 'expanded',
-    })).on('error', sass.logError).pipe(autoprefixer({
-    })).pipe(sourcemaps.write()).pipe(gulp.dest(paths.dist.css)).pipe(
-        cleanCss()).pipe(rename({
-        suffix: '.min'
-    })).pipe(gulp.dest(paths.dist.css));
-    done();
+  gulp.src(paths.src.scss).pipe(sourcemaps.init()).pipe(sass({
+    outputStyle: 'expanded',
+  })).on('error', sass.logError).pipe(autoprefixer({
+  })).pipe(sourcemaps.write()).pipe(gulp.dest(paths.dist.css)).pipe(
+    cleanCss()).pipe(rename({
+    suffix: '.min'
+  })).pipe(gulp.dest(paths.dist.css));
+  done();
 });
 
+gulp.task('default', () => {
+  gulp.watch(paths.src.scss, gulp.task('sass'));
+})
+
 gulp.task('dev', () => {
-    gulp.watch(paths.src.scss, gulp.task('sass'));
+  gulp.watch(paths.src.scss, gulp.task('sass'));
 });
