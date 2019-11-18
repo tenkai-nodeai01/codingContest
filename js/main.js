fa77b8d7ff266274ccaa8b641bea4d5ad38b5846
adhiyudanasvarajati <adhi@10-kai.jp>
2019-11-18 15:59:53 +0900

    add js for section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 window.onload = function () {
+  let asideShowEventListener = function () {
+    let headerEl = document.getElementsByClassName('log-list__header')
+    for (let x = 0; x < headerEl.length; x++) {
+      headerEl[x].addEventListener('click', function () {
+        headerEl[x].classList.toggle('is-opened')
+      })
+    }
+  }
 
+  asideShowEventListener()
 }
