a1c1f27a984863911c5a52795f36571aacec1140
adhiyudanasvarajati <adhi@10-kai.jp>
2019-12-02 19:21:31 +0900

    edit artwork page

--- js/artwork.js
+++ js/artwork.js
@@ -1,17 +1,16 @@
 window.onload = function () {
   let favButtonFunction = function () {
     let favIcon = this.document.getElementsByClassName('favorite-icon')[0]
-    const excessText = 'お気に入り'
 
     // お気に入りのボタン処理、押したら、他のアイコンに切り替え
     favIcon.addEventListener('click', function () {
       this.classList.toggle('is-liked')
-      let favTextEl = document.getElementsByClassName('favourite-count')[0]
-      let favTextNumber = Number(favTextEl.innerText.replace(excessText, ''))
+      let favTextEl = document.getElementsByClassName('favourite-count__number')[0]
+      let favTextNumber = Number(favTextEl.innerText)
       if (this.classList.contains('is-liked')) {
-        favTextEl.innerText = favTextNumber + 1 + excessText
+        favTextEl.innerText = favTextNumber + 1
       } else {
-        favTextEl.innerText = favTextNumber - 1 + excessText
+        favTextEl.innerText = favTextNumber - 1
       }
     })
   }
